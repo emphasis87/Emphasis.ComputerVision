93dcd1520f7fea2259210ca83be4ca3623578410
Luboš Hemala <lubos.hemala@gmail.com>
2021-05-08 17:39:51 +0200

    swt

---
+++ src/Emphasis.ComputerVision.OpenCL.StrokeWidthTransform/OclStrokeWidthTransform.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Emphasis.ComputerVision.OpenCL.StrokeWidthTransform
+{
+	public class OclStrokeWidthTransform
+	{
+		public static void StrokeWidthTransformExact(
+			nint queueId,
+			nint srcImageId,
+			nint dstImageId,
+			nint[] waitOnEvents,
+			out nint onFinishedEvent,
+			int rays = 1,
+			int rayBend = 15,
+			bool direction = true)
+		{
+
+		}
+		
+	}
+}
